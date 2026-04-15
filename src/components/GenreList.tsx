0b6c0011be767184331355a0d83b00cb27712849
Veteranlegend <113905752+Veteranlegend@users.noreply.github.com>
2026-04-15 19:01:35 +0200

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,15 @@
+import { ul } from 'framer-motion/client';
+import React from 'react'
+import useGenres from '../hooks/useGenres'
+
+const GenreList = () => {
+ const {genres} = useGenres(); 
+  
+    return (
+    <ul>
+    {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+    </ul>
+  )
+}
+
+export default GenreList
