05f728bc84378eb1c452d019d9f486ab02082bf9
Veteranlegend <113905752+Veteranlegend@users.noreply.github.com>
2026-04-16 10:10:09 +0200

    Handle games with no Image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,29 +1,37 @@
-
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import { Game } from '../hooks/useGames'
-import React from 'react'
-import PlatformiconList from './PlatformiconList'
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+import { useState } from "react";
+import PlatformiconList from "./PlatformiconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
+import noImage from "../assets/no-image-placeholder.webp";
 
-
-interface Props { 
-    game: Game
+interface Props {
+  game: Game;
 }
 
-const GameCard = ({game}: Props) => {
+const GameCard = ({ game }: Props) => {
+  const [imageSrc, setImageSrc] = useState(getCroppedImageUrl(game.background_image));
+
   return (
-    <Card>
-      <Image  src={game.background_image} />
-    <CardBody> 
-        <Heading fontSize= '2xl'>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-          <PlatformiconList platforms={game.parent_platforms.map(p=>p.platform)}  /> 
+    <Card borderRadius="10px" overflow="hidden">
+      <Image
+        src={imageSrc}
+        onError={() => setImageSrc(noImage)}
+      />
+
+      <CardBody>
+        <Heading fontSize={{ base: "lg", md: "xl", lg: "2xl" }}>
+          {game.name}
+        </Heading>
+
+        <HStack justifyContent="space-between">
+          <PlatformiconList platforms={game.parent_platforms.map((p) => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
-    </CardBody>
-
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
